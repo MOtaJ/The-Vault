5c59f3d4b864d209473cf5c971f1cd6531b53fa5
Mark Ota <markota@Marks-MacBook-Pro-2.local>
2016-11-23 17:49:24 -1000

    passed tests

--- vault.js
+++ vault.js
@@ -1,4 +1,26 @@
 'use strict';
 module.exports = function() {
-  
+
+    var lock = {};
+
+function setValue(key, value) {
+    lock[key] = value;
+    return lock;
+}
+
+function getValue(key) {
+  if (lock.hasOwnProperty(key)) {
+    return lock[key];
+  } else {
+    return null;
+  }
+
 };
+
+
+return{
+    setValue: setValue,
+    getValue: getValue
+};
+
+}
